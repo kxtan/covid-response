56b18849389c706e72c7056204145182c71cfa1c
chunwei <C140176@e.ntu.edu.sg>
2020-03-29 01:40:36 +0800

    added plot to plot daily change in confirmed cases, added a check box to check when school first started closing. TODO: add more checkboxes for workplace closing, campaign etc. Plot a vertical line instead of a freaking dot

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_countries(raw_df):
 def get_data_df(raw_df):
 
     primary_index_lst=["CountryName", "CountryCode", "Date"]
-    data_df = raw_df[primary_index_lst + ["StringencyIndex", "ConfirmedCases", "ConfirmedDeaths"]]    
+    data_df = raw_df[primary_index_lst + ["StringencyIndex", "ConfirmedCases", "ConfirmedDeaths", "S1_School closing"]]    
     #data_df = data_df.fillna(0)
 
     return data_df
@@ -39,21 +39,43 @@ def get_country_df(country, data_df):
 
     return grouped.get_group(selected_country)
 
+@st.cache
+def get_school_closed_df(country, data_df):
+    
+    school_closed_df = data_df[data_df.CountryName == country]
+    school_closed_df = school_closed_df[['CountryName', 'Date', 'S1_School closing']]
+    # The point in time where schools are closed
+    school_closed_df = school_closed_df[data_df['S1_School closing'] > 0]
+
+    school_closed_df = school_closed_df['Date'].min()
+    return school_closed_df
+
+@st.cache
+def get_daily_change_confirmed_case_df(country, data_df):
+    daily_change_df = data_df[data_df.CountryName == country]
+    daily_change_df = daily_change_df[['CountryName', 'Date', 'ConfirmedCases']]
+    daily_change_df['DailyChangeInConfirmedCases'] = daily_change_df['ConfirmedCases'].diff()
+    return daily_change_df
+
 st.title("Country Covid-19 Tracker")
 raw_df = read_file(str(dt.date.today()))
 
 data_df = get_data_df(raw_df)
 country_lst = get_countries(data_df)
 
-selected_country = st.selectbox("Select Country", 
+selected_country = st.sidebar.selectbox("Select Country", 
     country_lst, country_lst.index("United Kingdom"))
 
 target_df = get_country_df(selected_country, data_df)
 
-remove_zero = st.checkbox("Remove zero cases", value=True)
+remove_zero = st.sidebar.checkbox("Remove zero cases", value=True)
+
+school_closed = st.sidebar.checkbox("School closed", value=False)
 
 if remove_zero:
     target_df = target_df[(target_df[["StringencyIndex", "ConfirmedCases", "ConfirmedDeaths"]].T != 0).any()]
+
+daily_change_df = get_daily_change_confirmed_case_df(selected_country, data_df)
 
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 
@@ -82,6 +104,26 @@ fig.add_trace(
     secondary_y=True
 )
 
+fig.add_trace(
+    go.Scatter(
+        x=daily_change_df["Date"],
+        y=daily_change_df["DailyChangeInConfirmedCases"],
+        name="Daily changes of confirmed cases"
+    ),
+    secondary_y=True
+)
+
+if school_closed:
+    school_closed_date = get_school_closed_df(selected_country, data_df)
+
+    fig.add_trace(
+        go.Scatter(
+            x=target_df[target_df.Date == school_closed_date]['Date'],
+            y=daily_change_df[daily_change_df.Date == school_closed_date]['DailyChangeInConfirmedCases'],
+            name="School closed"
+        )
+    )
+fig.update_shapes(dict(xref='x', yref='y'))
 fig.update_layout(
     title="Daily change in Stringency, Cases & Deaths"
 )
