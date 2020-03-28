5a6f051dc118dacf0485ec74609f23a3f113d956
chunwei <C140176@e.ntu.edu.sg>
2020-03-28 23:26:21 +0800

    added main entry point in data_updater, and using heroku scheduler to run this daily to update our data. Also added some logs for performance tracking

--- data_updater.py
+++ data_updater.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+import datetime as dt
+import time
 
 def download_data(url):
     r = requests.get(url, 'data/data_in_excel.xlsx')
@@ -11,6 +13,11 @@ def convert_excel_to_csv(infile, outfile):
     excel_file = pd.read_excel(infile)
     excel_file.to_csv(outfile, index=None, header=True)
 
-# if __name__ == '__main__':
-#     download_data('https://www.bsg.ox.ac.uk/sites/default/files/OxCGRT_Download_latest_data.xlsx')
-#     convert_excel_to_csv('data/data_in_excel.xlsx', 'data/from_excel.csv')
+if __name__ == '__main__':
+
+    print('Downloading data...')
+    start = time.process_time()
+    download_data('https://www.bsg.ox.ac.uk/sites/default/files/OxCGRT_Download_latest_data.xlsx')
+    convert_excel_to_csv('data/data_in_excel.xlsx', 'data/oxcgrt.csv')
+    print('Downloaded data on {} using {} seconds'.format(dt.datetime.today(), time.process_time() - start))
+
