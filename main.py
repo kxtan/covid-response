6325934b51d4a0758613dff8d3ceaf4a459821d1
kxuen <tkxuen@gmail.com>
2020-03-29 14:35:56 +0800

    add daily delta chart
    ^

--- main.py
+++ main.py
@@ -27,18 +27,22 @@ def get_countries(raw_df):
 def get_data_df(raw_df):
 
     primary_index_lst=["CountryName", "CountryCode", "Date"]
-    data_df = raw_df[primary_index_lst + ["StringencyIndex", "ConfirmedCases", "ConfirmedDeaths", "S1_School closing"]]    
+    data_df = raw_df[primary_index_lst + 
+    ["StringencyIndex", "ConfirmedCases", "ConfirmedDeaths", "S1_School closing"]]    
     #data_df = data_df.fillna(0)
 
     return data_df
 
 @st.cache
-def get_country_df(country, data_df):
+def get_country_df(country, data_df, drop_countries=True):
 
     grouped = data_df.groupby("CountryName")
+    df = grouped.get_group(selected_country)
 
-    return grouped.get_group(selected_country)
+    if drop_countries:
+        df = df.drop(columns=["CountryName", "CountryCode"])
 
+    return df
 @st.cache
 def get_school_closed_df(country, data_df):
     
@@ -50,12 +54,6 @@ def get_school_closed_df(country, data_df):
     school_closed_df = school_closed_df['Date'].min()
     return school_closed_df
 
-@st.cache
-def get_daily_change_confirmed_case_df(country, data_df):
-    daily_change_df = data_df[data_df.CountryName == country]
-    daily_change_df = daily_change_df[['CountryName', 'Date', 'ConfirmedCases']]
-    daily_change_df['DailyChangeInConfirmedCases'] = daily_change_df['ConfirmedCases'].diff()
-    return daily_change_df
 
 st.title("Country Covid-19 Tracker")
 raw_df = read_file(str(dt.date.today()))
@@ -70,16 +68,18 @@ target_df = get_country_df(selected_country, data_df)
 
 remove_zero = st.sidebar.checkbox("Remove zero cases", value=True)
 
-school_closed = st.sidebar.checkbox("School closed", value=False)
+# school_closed = st.sidebar.checkbox("School closed", value=False)
 
 if remove_zero:
     target_df = target_df[(target_df[["StringencyIndex", "ConfirmedCases", "ConfirmedDeaths"]].T != 0).any()]
 
-daily_change_df = get_daily_change_confirmed_case_df(selected_country, data_df)
+###Get daily delta in df form
+delta_df = target_df[["ConfirmedCases", "ConfirmedDeaths"]].diff()
+delta_df.insert(0, 'Date', target_df["Date"])
 
-fig = make_subplots(specs=[[{"secondary_y": True}]])
+cumu_fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-fig.add_trace(
+cumu_fig.add_trace(
     go.Scatter(
         x=target_df["Date"],
         y=target_df["ConfirmedCases"],
@@ -87,7 +87,7 @@ fig.add_trace(
     )
 )
 
-fig.add_trace(
+cumu_fig.add_trace(
     go.Scatter(
         x=target_df["Date"],
         y=target_df["ConfirmedDeaths"],
@@ -95,7 +95,7 @@ fig.add_trace(
     )
 )
 
-fig.add_trace(
+cumu_fig.add_trace(
     go.Scatter(
         x=target_df["Date"],
         y=target_df["StringencyIndex"],
@@ -104,36 +104,74 @@ fig.add_trace(
     secondary_y=True
 )
 
-fig.add_trace(
+# if school_closed:
+#     school_closed_date = get_school_closed_df(selected_country, data_df)
+
+#     cumu_fig.add_trace(
+#         go.Scatter(
+#             x=target_df[target_df.Date == school_closed_date]['Date'],
+#             y=daily_change_df[daily_change_df.Date == school_closed_date]['DailyChangeInConfirmedCases'],
+#             name="School closed"
+#         )
+#     )
+
+cumu_fig.update_shapes(dict(xref='x', yref='y'))
+cumu_fig.update_layout(
+    title="Cumulative chart on Stringency, Cases & Deaths"
+)
+cumu_fig.update_xaxes(title_text="Date")
+cumu_fig.update_yaxes(title_text="Cases/Deaths", secondary_y=False)
+cumu_fig.update_yaxes(title_text="StringencyIndex", secondary_y=True)
+
+st.write(cumu_fig)
+
+st.subheader("Cumulative Table: ")
+display_columns = ["Date", "ConfirmedCases", 
+    "ConfirmedDeaths", "StringencyIndex"]
+st.write(target_df[display_columns].set_index("Date").sort_index(ascending=False))
+
+delta_fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+delta_fig.add_trace(
     go.Scatter(
-        x=daily_change_df["Date"],
-        y=daily_change_df["DailyChangeInConfirmedCases"],
-        name="Daily changes of confirmed cases"
+        x=delta_df["Date"],
+        y=delta_df["ConfirmedCases"],
+        name="ConfirmedCases"
+    )
+)
+
+delta_fig.add_trace(
+    go.Scatter(
+        x=delta_df["Date"],
+        y=delta_df["ConfirmedDeaths"],
+        name="ConfirmedDeaths"
+    )
+)
+
+delta_fig.add_trace(
+    go.Scatter(
+        x=target_df["Date"],
+        y=target_df["StringencyIndex"],
+        name="StringencyIndex"
     ),
     secondary_y=True
 )
 
-if school_closed:
-    school_closed_date = get_school_closed_df(selected_country, data_df)
-
-    fig.add_trace(
-        go.Scatter(
-            x=target_df[target_df.Date == school_closed_date]['Date'],
-            y=daily_change_df[daily_change_df.Date == school_closed_date]['DailyChangeInConfirmedCases'],
-            name="School closed"
-        )
-    )
-fig.update_shapes(dict(xref='x', yref='y'))
-fig.update_layout(
-    title="Daily change in Stringency, Cases & Deaths"
+delta_fig.update_layout(
+    title="Daily Change in Cases & Deaths"
 )
-fig.update_xaxes(title_text="Date")
-fig.update_yaxes(title_text="Cases/Deaths", secondary_y=False)
-fig.update_yaxes(title_text="StringencyIndex", secondary_y=True)
+delta_fig.update_xaxes(title_text="Date")
+delta_fig.update_yaxes(title_text="Cases/Deaths", secondary_y=False)
+delta_fig.update_yaxes(title_text="StringencyIndex", secondary_y=True)
 
-st.write(fig)
-st.write(target_df[["Date", "StringencyIndex", "ConfirmedCases", "ConfirmedDeaths"]]\
-    .set_index("Date").sort_index(ascending=False))
+st.write(delta_fig)
+
+st.subheader("Daily Changes Table: ")
+display_columns = ["Date", "ConfirmedCases", 
+    "ConfirmedDeaths"]
+display_delta_df = delta_df.rename(columns={"ConfirmedCases": "NewCases", "ConfirmedDeaths": "NewDeaths"})
+display_delta_df = display_delta_df.set_index("Date").sort_index(ascending=False)
+st.write(display_delta_df)
 
 st.subheader("Disclaimer:")
 st.text(" Please note that the  Stringency Index is for comparative purposes only, and should not\
